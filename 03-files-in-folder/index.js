b15fab53730af2ace922a835936122a24fe71941
Mikhail <mishania261282@mail.ru>
2024-01-22 22:21:29 +0300

    fix:fix checking for a file in the 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,24 +3,26 @@ const path = require('path');
 const { stdin, stdout } = process;
 const pathFolder = path.join(__dirname, 'secret-folder');
 
-fs.readdir(pathFolder, (error, files) => {
+fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
   if (error) {
     console.log(error);
   }
 
   files.forEach((file) => {
-    const pathFile = path.join(pathFolder, file);
-    fs.stat(pathFile, (error, stats) => {
-      if (error) {
-        console.log(error);
-      } else {
-        const extension = path.extname(file);
-        const name = path.basename(file, extension);
-        stdout.write(
-          `${name}-${extension.slice(1)}-${(stats.size / 1024).toFixed(2)}kb\n`,
-        );
-        // console.log(`${name}-${extension}-${(stats.size / 1024).toFixed(2)}kb`);
-      }
-    });
+    if (file.isFile()) {
+      const pathFile = path.join(pathFolder, file.name);
+      fs.stat(pathFile, (error, stats) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const extension = path.extname(file.name);
+          const name = path.basename(file.name, extension);
+          stdout.write(
+            `${name}-${extension.slice(1)}-${(stats.size / 1024).toFixed(2)}kb\n`,
+          );
+          // console.log(`${name}-${extension}-${(stats.size / 1024).toFixed(2)}kb`);
+        }
+      });
+    }
   });
 });
